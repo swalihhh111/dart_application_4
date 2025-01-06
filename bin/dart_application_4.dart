67b96d0eb82dfc9ae8b7d4ee01cb77ae362e4c47
swalihhh111 <muhaswalih110@gmail.com>
2025-01-06 12:38:13 +0530

    first commit

---
+++ bin/dart_application_4.dart
@@ -0,0 +1,145 @@
+import 'dart:io';
+
+void main() {
+  // stdout.write("Enter a number");
+  // String input1 = stdin.readLineSync()!;
+  // int num1 = int.parse(input1);
+
+  // for (int i = 1; i <= 10; i++) {
+  //   print("$num1 * $i =${num1 * i}");
+  // }
+
+  // stdout.write("Enter a number:");
+  // String input1 = stdin.readLineSync()!;
+  // int num1 = int.parse(input1);
+
+  // int sum =0;
+  // for (int i = 1; i <= num1; i++) {
+  //   sum += i;
+  // }
+  // print("sum of  number is $sum");
+
+//   int num1 = 0;
+//   int sum = 0;
+//   int input = 10;
+//  for(int i =1; i<=input;i++){
+//   if(i%2==0){
+
+  // }
+//
+// }
+
+  // String value1 = "swalih";
+  // String reValue = value1.split("").reversed.join();
+
+  // print(reValue);
+  // if (value1 == reValue) {
+  //   print("the word is palindrome");
+  // } else {
+  //   print("the word palindrome");
+  // }
+//..................................................................................................//
+  //
+  // int i, j, l, k;
+  // for (i = 1; i <= 4; i++) {
+  //   if (i % 2 == 1) {
+  //     l = 1;
+  //   } else {
+  //     l = 3;
+  //   }
+  //   for (j = 1; j <= l; j++) {
+  //     print("*");
+  //   }
+  //   if (i == 4) {
+  //     break;
+  //   }
+  //   for (k = 1; k <= 4 * i; k++) {
+  //     stdout.write("*");
+  //   }
+  //   print("");
+  // }
+
+  // int i, j;
+  // for (i = 1; i <= 5; i++) {
+  //   for (j = 1; j <= 5; j++) {
+  //     stdout.write("* ");
+  //   }
+  //   stdout.write("\n");
+  // }
+
+  // for (int i = 1; i <= 5; i++) {
+  //   for (int j = 1; j <= 5; j++) {
+  //     stdout.write("$i ");
+  //   }
+  //   print("");
+  // }
+
+  // for (int i = 1; i <= 5; i++) {
+  //   for (int j = 1; j <= 5; j++) {
+  //     if (i == 1 || i == 5 || j == 1 || j == 5) {
+  //       stdout.write("* ");
+  //     } else {
+  //       stdout.write("  ");
+  //     }
+  //   }
+  //   print("");
+  // }
+
+  //..........................................
+  // int count = 0;
+  // for (int i = 1; i <= 5; i++) {
+  //   for (int j = 1; j <= 5; j++) {
+  //     if (count < 9) {
+  //       stdout.write("0${count += 1} ");
+  //     } else {
+  //       stdout.write("${count += 1} ");
+  //     }
+  //   }
+  //   print("");
+  // }
+  //..........................................
+  // for (int i = 1; i <= 5; i++) {
+  //   for (int j = 1; j <= i; j++) {
+  //     if (j == 1 || i == 5) {
+  //       stdout.write("*");
+  //     } else {
+  //       stdout.write("\n");
+  //     }
+  //   }
+  //   print("");
+  // }
+
+  // stdout.write("Enter a Number:");
+  // int count = int.parse(stdin.readLineSync()!);
+
+  // for (int i = 1; i <= count; i++) {
+  //   for (int j = 1; j <= i; j++) {
+  //     stdout.write("* ");
+  //   }
+  //   stdout.write("\n");
+  // }
+
+  stdout.write("Enter a Number:");
+  int num1 = int.parse(stdin.readLineSync()!);
+
+  for (int i = 1; i <= num1; i++) {
+    for (int j = 1; j <= num1; j++) {
+      if (i == 1 || j == 1 || j == num1) {
+        stdout.write("* ");
+      } else {
+        stdout.write("  ");
+      }
+    }
+    stdout.write("\n");
+  }
+
+  // stdout.write("Enter a Number:");
+  // int num1 = int.parse(stdin.readLineSync()!);
+
+  // for (num1; num1 >= 1; num1--) {
+  //   for (int j = 1; j <= num1; j++) {
+  //     stdout.write("* ");
+  //   }
+  //   print("");
+  // }
+}
